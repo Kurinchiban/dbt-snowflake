fceaa9aba33dfcd84807bc7fdea6f3f058568388
Kurinchiban <kurinchiban003@gmail.com>
2024-12-09 12:02:38 +0530

    add snapshot

---
+++ transform_student_dataset/snapshots/marks_snapshot.sql
@@ -0,0 +1,15 @@
+{% snapshot mark_snapshot %}  
+
+{{    
+  config(      
+    target_schema='public',      
+    strategy='check',      
+    unique_key='studentId',      
+    check_cols=['status']    
+  )  
+}}  
+
+select * 
+from {{ source('student', 'students_marks') }}
+
+{% endsnapshot %}
