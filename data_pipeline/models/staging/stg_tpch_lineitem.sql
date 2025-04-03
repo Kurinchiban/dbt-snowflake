b6ed1c1939784ad3812d09936059012ecae5e6f2
Kurinchiban <kurinchiban003@gmail.com>
2025-04-03 19:54:00 +0530

    data pipeline

---
+++ data_pipeline/models/staging/stg_tpch_lineitem.sql
@@ -0,0 +1,17 @@
+with line_item as (
+    select 
+        {{ dbt_utils.generate_surrogate_key([
+            'l_orderkey',
+            'l_linenumber'
+        ]) }} as order_item_key,
+        l_orderkey as order_key,
+        l_partkey as part_key,
+        l_linenumber as line_number,
+        l_quantity as quantity,
+        l_extendedprice as extended_price,
+        l_discount as discount_percentage,
+        l_tax as tax_rate
+    from {{ source('tpch', 'lineitem') }}
+)
+
+select * from line_item
